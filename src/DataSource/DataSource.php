327746118d34377c3d427a71d169725511277c49
Andrew Masiye <andrew.masiye@liferaftinc.com>
2022-07-26 08:30:12 +0200

    feat(datasource): create DataSource management classes

---
+++ src/DataSource/DataSource.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Assegai\Orm\DataSource;
+
+use Assegai\Orm\Enumerations\DataSourceType;
+use Assegai\Orm\Exceptions\ClassNotFoundException;
+use Assegai\ORM\Exceptions\DataSourceException;
+use Assegai\Orm\Exceptions\IllegalTypeException;
+use Assegai\Orm\Interfaces\IRepository;
+use Assegai\Orm\Management\EntityManager;
+use JetBrains\PhpStorm\ArrayShape;
+use PDO;
+use ReflectionClass;
+use ReflectionException;
+
+class DataSource
+{
+  public readonly EntityManager $manager;
+  public readonly PDO $db;
+  public readonly DataSourceType $type;
+
+  /**
+   * @throws DataSourceException
+   */
+  #[ArrayShape([
+    'entities' => 'array',
+    'database' => 'string',
+    'type' => 'Assegai\Orm\Enumerations\DataSourceType',
+    'host' => 'string',
+    'port' => 'int',
+    'username' => 'string|null',
+    'password' => 'string|null'
+  ])]
+  public function __construct(DataSourceOptions|array|null $options = null)
+  {
+//    exit(var_export(['options' => $options], true) . PHP_EOL);
+    $reflectionClass = new ReflectionClass($this);
+    $refAttributes = $reflectionClass->getAttributes(DataSourceOptions::class);
+
+    if (empty($options) && empty($refAttributes))
+    {
+      throw new DataSourceException("DataSourceOptions not set");
+    }
+
+//    /** @var ReflectionAttribute $refOptions */
+//    $refOptions = array_pop($refAttributes);
+//
+//    /** @var DataSourceOptions $options */
+//    $options = $refOptions->newInstance();
+
+    $this->type = $options->type;
+
+    // TODO: #80 Check if the specified databases is in config @amasiye
+    if (
+      !empty($options->database) &&
+      !empty($options->username) &&
+      !empty($options->password) &&
+      !empty($options->port)
+    )
+    {
+      $host = $options->host;
+      $name = $options->database;
+      $port = $options->port;
+
+      $dsn = match ($this->type) {
+        DataSourceType::POSTGRESQL => "pgsql:host=$host;port=$port;dbname=$name",
+        DataSourceType::MSSQL => "sqlsrv:Server=$host,port;Database=$name",
+        DataSourceType::SQLITE => "sqlite:$name",
+        default => "mysql:host=$host;port=$port;dbname=$name"
+      };
+
+      $this->db = new PDO(dsn: $dsn, username: $options->username, password: $options->password);
+    }
+    else
+    {
+      $this->db = match ($this->type) {
+        DataSourceType::POSTGRESQL  => DBFactory::getPostgresSQLConnection(dbName: $options->database),
+        DataSourceType::SQLITE      => DBFactory::getSQLiteConnection(dbName: $options->database),
+        DataSourceType::MONGODB     => DBFactory::getMongoDbConnection(dbName: $options->database),
+        DataSourceType::MARIADB,
+        DataSourceType::MYSQL       => DBFactory::getMySQLConnection(dbName: $options->database),
+        default                     => DBFactory::getSQLConnection(dbName: $options->database)
+      };
+    }
+
+//    exit(var_export(['connection' => $this], true) . PHP_EOL);
+    $this->manager = new EntityManager(connection: $this);
+  }
+
+  /**
+   * @param string $entityName The target entity for the repository
+   * @return IRepository
+   * @throws ClassNotFoundException
+   * @throws IllegalTypeException
+   * @throws ReflectionException
+   */
+  public function getRepository(string $entityName): IRepository
+  {
+    if (!class_exists($entityName))
+    {
+      throw new ClassNotFoundException(className: $entityName);
+    }
+
+    return new Repository(entityId: $entityName, manager: $this->manager);
+  }
+}
---
+++ src/DataSource/DataSourceOptions.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Assegai\Orm\DataSource;
+
+use Assegai\Orm\Enumerations\DataSourceType;
+
+class DataSourceOptions
+{
+  /**
+   * @param array $entities
+   * @param string $database
+   * @param DataSourceType $type
+   * @param string $host
+   * @param int $port
+   * @param string|null $username
+   * @param string|null $password
+   */
+  public function __construct(
+    public readonly array $entities,
+    public readonly string $database,
+    public readonly DataSourceType $type = DataSourceType::MYSQL,
+    public readonly string $host = 'localhost',
+    public readonly int $port = 3306,
+    public readonly ?string $username = null,
+    public readonly ?string $password = null,
+  )
+  {
+  }
+}
