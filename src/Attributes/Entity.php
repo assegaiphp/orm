5e6c25d7397f5a6670dd8d2c78cc83e35684fb1c
Andrew Masiye <andrew.masiye@liferaftinc.com>
2022-07-26 08:19:10 +0200

    refactor(attributes): add parameters to the Entity attribute

--- src/Attributes/Entity.php
+++ src/Attributes/Entity.php
@@ -2,6 +2,23 @@
 
 namespace Assegai\Orm\Attributes;
 
+use Assegai\Orm\Enumerations\DataSourceType;
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS)]
 class Entity
 {
+  public function __construct(
+    public ?string         $table = null,
+    public ?string         $orderBy = null,
+    public ?string         $engine = null,
+    public ?string         $database = null,
+    public ?string         $schema = null,
+    public ?bool           $synchronize = true,
+    public ?bool           $withRowId = false,
+    public ?array          $protected = ['password'],
+    public ?DataSourceType $driver = DataSourceType::MYSQL,
+  )
+  {
+  }
 }
