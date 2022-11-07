a55b0cf1d90f8a3769b55fe3749d253ac72b9f4d
Andrew Masiye <andrew.masiye@liferaftinc.com>
2022-11-08 01:03:35 +0200

    feat(): create column attributes

---
+++ src/Attributes/Relations/ManyToOne.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Assegai\Orm\Attributes\Relations;
+
+use Assegai\Orm\Relations\RelationsOptions;
+use Attribute;
+
+/**
+ * A many-to-one is a relation where A contains multiple instances of B, but B contains only one instance of A.
+ */
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class ManyToOne
+{
+  /**
+   * @param string $type
+   * @param string|null $name
+   * @param string|null $alias
+   * @param RelationsOptions|null $options
+   */
+  public function __construct(
+    public readonly string $type,
+    public readonly ?string $name = null,
+    public readonly ?string $alias = null,
+    public ?RelationsOptions $options = null
+  )
+  {
+    if (is_null($this->options))
+    {
+      $this->options = new RelationsOptions();
+    }
+  }
+}
---
+++ src/Attributes/Relations/OneToOne.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Assegai\Orm\Attributes\Relations;
+
+use Assegai\Orm\Exceptions\ClassNotFoundException;
+use Assegai\Orm\Relations\RelationsOptions;
+use Attribute;
+
+/**
+ * One-to-one is a relation where A contains only one instance of B and B
+ * contains only one instance of A. For example, if we had `User` and
+ * `Profile` entities, `User` can have only a single `Profile`, while a
+ * single `Profile` is owned by one `User`.
+ */
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class OneToOne
+{
+  /**
+   * @param string $type
+   * @param string|null $name
+   * @param string|null $alias
+   * @param RelationsOptions|null $options
+   * @throws ClassNotFoundException
+   */
+  public function __construct(
+    public string $type,
+    public ?string $name = null,
+    public ?string $alias = null,
+    public ?RelationsOptions $options = null
+  )
+  {
+    if (is_null($this->options))
+    {
+      $this->options = new RelationsOptions();
+    }
+
+    if (!class_exists($type))
+    {
+      throw new ClassNotFoundException(className: $type);
+    }
+  }
+}
