77f78520bc6c596c356997fe45f065be2bdeab17
Andrew Masiye <andrew.masiye@liferaftinc.com>
2023-02-22 10:43:29 +0200

    feat(metadata): create SQLTableDescription.php
    * created a data class to describe table field descriptions

---
+++ src/Metadata/SQLTableDescription.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Assegai\Orm\Metadata;
+
+/**
+ * The SQLTableDescription class represents the description of a table's column in a database.
+ * It contains information such as the column name, data type, whether it allows null values,
+ * if it's a key, the default value, and any additional information.
+ */
+class SQLTableDescription
+{
+  /** @var string|null The name of the field. */
+  public ?string $Field = '';
+  /** @var string|null The data type of the field. */
+  public ?string $Type = '';
+  /** @var string|null Whether the field can contain null values. */
+  public ?string $Null = '';
+  /** @var string|null The type of index on the field, if any. */
+  public ?string $Key = '';
+  /** @var string|null The default value of the field. */
+  public ?string $Default = '';
+  /** @var string|null Any additional information about the field. */
+  public ?string $Extra = '';
+}
