39649dc1ac16f0823e1a57cf358c3fba14c856ac
Andrew Masiye <andrew.masiye@liferaftinc.com>
2023-02-28 23:13:56 +0200

    feat(migrations): create SchemaMigrationsEntity.php
    The SchemaMigrationsEntity class defines the structure of the `__assegai_schema_migrations` table which stores information about which migrations have been run.

---
+++ src/Migrations/SchemaMigrationsEntity.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Assegai\Orm\Migrations;
+
+use Assegai\Orm\Attributes\Columns\Column;
+use Assegai\Orm\Attributes\Entity;
+use Assegai\Orm\Queries\Sql\ColumnType;
+
+#[Entity(table: '__assegai_schema_migrations')]
+class SchemaMigrationsEntity
+{
+  #[Column(type: ColumnType::VARCHAR, lengthOrValues: 50, nullable: false, isPrimaryKey: true)]
+  public string $migration = '';
+
+  #[Column(name: 'ran_on', type: ColumnType::DATETIME, nullable: false, default: 'CURRENT_TIMESTAMP')]
+  public string $ranOn = '';
+}
