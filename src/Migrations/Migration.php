179c27ef2a3cc847db885b11a4034559423093fa
Andrew Masiye <andrew.masiye@liferaftinc.com>
2023-03-01 01:19:12 +0200

    refactor(migrations): improve the focus of the Migration.php class

--- src/Migrations/Migration.php
+++ src/Migrations/Migration.php
@@ -2,6 +2,7 @@
 
 namespace Assegai\Orm\Migrations;
 
+use Assegai\Orm\DataSource\DataSource;
 use Assegai\Orm\Interfaces\IMigration;
 
 /**
@@ -12,14 +13,32 @@ abstract class Migration implements IMigration
   /**
    * Abstract method that should be implemented to specify database migrations to run when upgrading the database
    *
+   * @param DataSource $dataSource The DataSource to be used for migrations.
    * @return void
    */
-  public abstract function up(): void;
+  public abstract function up(DataSource $dataSource): void;
 
   /**
    * Abstract method that should be implemented to specify database migrations to run when downgrading the database
    *
+   * @param DataSource $dataSource The DataSource to be used for migrations.
    * @return void
    */
-  public abstract function down(): void;
+  public abstract function down(DataSource $dataSource): void;
+
+  /**
+   * Get the name of the migration.
+   *
+   * This method retrieves the name of the migration by extracting the file name of the current migration class file.
+   *
+   * @return string The name of the migration.
+   */
+  protected function getName(): string
+  {
+    // Get the path information of the current file.
+    $info = pathinfo(__FILE__);
+
+    // Return the file name of the current migration class.
+    return $info['filename'];
+  }
 }
