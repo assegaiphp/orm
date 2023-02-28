21666635e757c582da256023129847cb7a0f92c6
Andrew Masiye <andrew.masiye@liferaftinc.com>
2023-02-28 23:10:25 +0200

    style(metadata): reformat SQLTableDescription.php file
    * added line breaks between class property declarations

--- src/Metadata/SQLTableDescription.php
+++ src/Metadata/SQLTableDescription.php
@@ -11,14 +11,19 @@ class SQLTableDescription
 {
   /** @var string|null The name of the field. */
   public ?string $Field = '';
+
   /** @var string|null The data type of the field. */
   public ?string $Type = '';
+
   /** @var string|null Whether the field can contain null values. */
   public ?string $Null = '';
+
   /** @var string|null The type of index on the field, if any. */
   public ?string $Key = '';
+
   /** @var string|null The default value of the field. */
   public ?string $Default = '';
+
   /** @var string|null Any additional information about the field. */
   public ?string $Extra = '';
 }
