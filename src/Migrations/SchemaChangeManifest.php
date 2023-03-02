ea45e61af916737550048c35bb5d5c263f4e432d
Andrew Masiye <andrew.masiye@liferaftinc.com>
2023-03-02 19:20:38 +0200

    feat(migrations): implement add, change and drop methods
    ## New Methods
    * add - Adds a new DDLAddStatement to the addList array
    * change - Adds a new DDLChangeStatement to the changeList array
    * drop - Adds a new DDLDropStatement to the dropList array

--- src/Migrations/SchemaChangeManifest.php
+++ src/Migrations/SchemaChangeManifest.php
@@ -3,6 +3,9 @@
 namespace Assegai\Orm\Migrations;
 
 use Assegai\Orm\Attributes\Entity;
+use Assegai\Orm\Queries\DDL\DDLAddStatement;
+use Assegai\Orm\Queries\DDL\DDLChangeStatement;
+use Assegai\Orm\Queries\DDL\DDLDropStatement;
 use Stringable;
 
 /**
@@ -12,21 +15,84 @@ use Stringable;
  *
  * @package Assegai\Orm\Migrations
  */
-readonly class SchemaChangeManifest implements Stringable
+class SchemaChangeManifest implements Stringable
 {
   /**
    * @param Entity $entity The entity for which schema changes are being made.
-   * @param array $addList An array of DDL statements to add new columns to the entity's schema.
-   * @param array $updateList An array of DDL statements to update existing columns in the entity's schema.
-   * @param array $dropList An array of DDL statements to drop columns from the entity's schema.
+   * @param DDLAddStatement[] $addList An array of DDL statements to add new columns to the entity's schema.
+   * @param DDLChangeStatement[] $changeList An array of DDL statements to change existing columns in the entity's schema.
+   * @param DDLDropStatement[] $dropList An array of DDL statements to drop columns from the entity's schema.
    */
   public function __construct(
-    public Entity $entity,
-    public array $addList = [],
-    public array $updateList = [],
-    public array $dropList = [],
+    public readonly Entity $entity,
+    protected array $addList = [],
+    protected array $changeList = [],
+    protected array $dropList = [],
   )
   {}
+
+  /**
+   * Adds a new DDLAddStatement to the $addList array.
+   *
+   * @param DDLAddStatement $statement A DDLAddStatement to be added to the array.
+   * @return void
+   */
+  public function add(DDLAddStatement $statement): void
+  {
+    $this->addList[] = $statement;
+  }
+
+  /**
+   * Adds a new DDLChangeStatement to the $changeList array.
+   *
+   * @param DDLChangeStatement $statement A DDLChangeStatement to be added to the array.
+   * @return void
+   */
+  public function change(DDLChangeStatement $statement): void
+  {
+    $this->changeList[] = $statement;
+  }
+
+  /**
+   * Adds a new DDLDropStatement to the $dropList array.
+   *
+   * @param DDLDropStatement $statement A DDLDropStatement to be added to the array.
+   * @return void
+   */
+  public function drop(DDLDropStatement $statement): void
+  {
+    $this->dropList[] = $statement;
+  }
+
+  /**
+   * Returns the $addList array.
+   *
+   * @return array An array of DDLAddStatement objects.
+   */
+  public function getAddList(): array
+  {
+    return $this->addList;
+  }
+
+  /**
+   * Returns the $changeList array.
+   *
+   * @return array An array of DDLChangeStatement objects.
+   */
+  public function getChangeList(): array
+  {
+    return $this->changeList;
+  }
+
+  /**
+   * Returns the $dropList array.
+   *
+   * @return array An array of DDLChangeStatement objects.
+   */
+  public function getDropList(): array
+  {
+    return $this->dropList;
+  }
 
   /**
    * Returns a string representation of the schema change manifest.
@@ -39,23 +105,23 @@ readonly class SchemaChangeManifest implements Stringable
   public function __toString(): string
   {
     $tableName = $this->entity->table;
-    $dataDefinitionStatement = "ALTER TABLE $tableName (";
+    $dataDefinitionStatement = "ALTER TABLE $tableName ";
 
     foreach ($this->addList as $addDDLStatement)
     {
-      $dataDefinitionStatement .= "ADD $addDDLStatement, ";
+      $dataDefinitionStatement .= "$addDDLStatement, ";
     }
 
-    foreach ($this->updateList as $changeDDLStatement)
+    foreach ($this->changeList as $changeDDLStatement)
     {
-      $dataDefinitionStatement .= "CHANGE $changeDDLStatement, ";
+      $dataDefinitionStatement .= "$changeDDLStatement, ";
     }
 
     foreach ($this->dropList as $dropDDLStatement)
     {
-      $dataDefinitionStatement .= "DROP $dropDDLStatement, ";
+      $dataDefinitionStatement .= "$dropDDLStatement, ";
     }
 
-    return trim($dataDefinitionStatement, " \t\n\r\0\x0B,") . ")";
+    return trim($dataDefinitionStatement, " \t\n\r\0\x0B,");
   }
 }
