70def2a78efbb260c75df3e0adc6d6c815c2c146
Andrew Masiye <andrew.masiye@liferaftinc.com>
2023-03-04 00:16:45 +0200

    docs(metadata): add documenting comments to SchemaMetadata.php
    This class represents metadata about a database schema. It is a readonly class, so its properties cannot be modified after the object has been created.
    
    The class has a constructor that accepts two parameters: an array of SQLTableDescription objects containing information about each table in the schema, and a string representing the DDL statement used to create the schema.

--- src/Metadata/SchemaMetadata.php
+++ src/Metadata/SchemaMetadata.php
@@ -2,11 +2,16 @@
 
 namespace Assegai\Orm\Metadata;
 
+/**
+ * Represents metadata about a database schema.
+ */
 readonly class SchemaMetadata
 {
   /**
-   * @param SQLTableDescription[] $tableFields
-   * @param string $ddlStatement
+   * Constructs a new SchemaMetadata object.
+   *
+   * @param SQLTableDescription[] $tableFields An array of SQLTableDescription objects containing information about each table in the schema.
+   * @param string $ddlStatement The DDL statement that was used to create the schema.
    */
   public function __construct(
     public array  $tableFields,
