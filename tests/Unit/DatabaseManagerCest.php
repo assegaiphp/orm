eb86cc1e121f400d503c81ccad219b8828ab6b35
Andrew Masiye <andrew.masiye@liferaftinc.com>
2023-03-05 00:42:46 +0200

    chore(tests): implemented test for DatabaseManager::drop method

--- tests/Unit/DatabaseManagerCest.php
+++ tests/Unit/DatabaseManagerCest.php
@@ -9,6 +9,7 @@ use Assegai\Orm\Enumerations\DataSourceType;
 use Assegai\ORM\Exceptions\DataSourceException;
 use Assegai\Orm\Exceptions\NotImplementedException;
 use Assegai\Orm\Management\DatabaseManager;
+use PDOException;
 use Tests\Support\UnitTester;
 
 class DatabaseManagerCest
@@ -62,13 +63,65 @@ class DatabaseManagerCest
     $I->assertFalse($this->databaseManager->exists($this->dataSource, $nonExistentDatabaseTableName));
   }
 
+  /**
+   * @param UnitTester $I
+   * @return void
+   * @throws DataSourceException
+   */
   public function testTheDropMethod(UnitTester $I): void
   {
-    throw new NotImplementedException(__METHOD__);
+    $dbName = 'zulu_db';
+    $this->createDatabase($dbName);
+    $this->createUsersTable($dbName);
+    $I->amConnectedToDatabase($dbName);
+    $I->haveInDatabase("$dbName.bantu", [
+      'username' => 'kingshak1787',
+      'email' => 'shaka.kaSenzangakhona@assegaiphp.com',
+      'full_name' => 'Shaka Zulu'
+    ]);
+
+    $this->databaseManager->drop($this->dataSource, $dbName);
+    $I->assertFalse($this->databaseManager->exists($this->dataSource, $dbName));
   }
 
   public function testTheResetMethod(UnitTester $I): void
   {
     throw new NotImplementedException(__METHOD__);
+  }
+
+  private function createDatabase(string $dbName): void
+  {
+    $sql = "CREATE DATABASE IF NOT EXISTS `$dbName` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci";
+    $statement = $this->dataSource->db->query($sql);
+
+    if (false === $statement->execute())
+    {
+      throw new PDOException("Failed to create database $dbName");
+    }
+  }
+
+  /**
+   * @param string $dbName
+   * @return void
+   */
+  private function createUsersTable(string $dbName): void
+  {
+    $sql = <<<MYSQL
+CREATE TABLE IF NOT EXISTS `$dbName`.`bantu` (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  full_name VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+MYSQL;
+    $statement = $this->dataSource->db->query($sql);
+
+    if (false === $statement->execute())
+    {
+      throw new PDOException("Failed to create users table in database $dbName");
+    }
   }
 }
