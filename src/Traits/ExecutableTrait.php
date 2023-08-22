7f52bde257983084d361f4b4b0aab65b66b03d50
Andrew Masiye <amasiye313@gmail.com>
2023-08-22 02:02:42 +0200

    docs: add documenting comments to the ExecutableTrait.php

--- src/Traits/ExecutableTrait.php
+++ src/Traits/ExecutableTrait.php
@@ -2,15 +2,32 @@
 
 namespace Assegai\Orm\Traits;
 
+use Assegai\Orm\Exceptions\ORMException;
 use Assegai\Orm\Queries\Sql\SQLQueryResult;
 
+/**
+ * Trait ExecutableTrait. Provides the execute method for queries.
+ *
+ * @package Assegai\Orm\Traits
+ */
 trait ExecutableTrait
 {
+  /**
+   * Executes the query and returns the result
+   *
+   * @return SQLQueryResult The result of the query.
+   * @throws ORMException If the query fails to execute.
+   */
   public function execute(): SQLQueryResult
   {
     return $this->query->execute();
   }
 
+  /**
+   * Exits the script and prints the query.
+   *
+   * @return never
+   */
   public function debug(): never
   {
     $this->query->debug();
