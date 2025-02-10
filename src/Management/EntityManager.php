22363f5c0b84246a04ab814da485bd0458f10f6c
Andrew Masiye <amasiye313@gmail.com>
2025-02-11 00:09:44 +0200

    fix(management): fix Repository::save call signature
    * Made options parameter `null` by default.

--- src/Management/EntityManager.php
+++ src/Management/EntityManager.php
@@ -201,7 +201,7 @@ class EntityManager implements IEntityStoreOwner
           $this->logger->warning("InsertOptions not provided. Using default InsertOptions.");
           $options = new InsertOptions();
         }
-        $saveResult = $this->insert(entityClass: $targetOrEntity::class, entity: $targetOrEntity, $options);
+        $saveResult = $this->insert(entityClass: $targetOrEntity::class, entity: $targetOrEntity, options: $options);
       } else if ($this->findBy($targetOrEntity::class, new FindWhereOptions(conditions: ['id' => $targetOrEntity->id]))) {
         if (!$options instanceof UpdateOptions) {
           $this->logger->warning("UpdateOptions not provided. Using default UpdateOptions.");
