93167da4f55c44f224cffcad5a60c8b3671eb4be
Andrew Masiye <amasiye313@gmail.com>
2025-02-15 00:22:44 +0200

    feat(management): add isDebug flag to InsertOptions.php

--- src/Management/Options/InsertOptions.php
+++ src/Management/Options/InsertOptions.php
@@ -4,16 +4,20 @@ namespace Assegai\Orm\Management\Options;
 
 /**
  * Options for insert operations.
+ *
+ * @package Assegai\Orm\Management\Options
  */
 readonly class InsertOptions
 {
   /**
    * InsertOptions constructor.
    *
-   * @param object|array|null $relations
+   * @param object|string[]|null $relations The relations to include.
+   * @param bool $isDebug Whether to output debug information.
    */
   public function __construct(
     public object|array|null $relations = null,
+    public bool $isDebug = false
   )
   {
   }
