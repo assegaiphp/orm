ffd204a9aab6cd80cdf438e0a24a66f8a5922b65
Andrew Masiye <amasiye313@gmail.com>
2025-03-30 00:00:34 +0200

    fix(traits): fix SQLAggregatorTrait.php
    * Fixed issue where order by always sorts in ascending order

--- src/Queries/Sql/SQLKeyPart.php
+++ src/Queries/Sql/SQLKeyPart.php
@@ -2,14 +2,30 @@
 
 namespace Assegai\Orm\Queries\Sql;
 
-final class SQLKeyPart
+use JsonSerializable;
+
+/**
+ * Represents a key part of an SQL query.
+ *
+ * This class is used to construct SQL queries with specific key parts,
+ * including optional sorting specifications (ascending or descending).
+ */
+final class SQLKeyPart implements JsonSerializable
 {
-  private string $queryString = '';
   /**
+   * The SQL string representation of the key part.
+   *
+   * @var string The SQL string representation of the key part.
+   */
+  private string $queryString = '';
+
+  /**
+   * SQLKeyPart constructor.
+   *
    * @param string $key The name of the key part
    * @param bool|null $ascending Sort specifier. If set to `true`, appends 
    * `ASC` to resulting Sql. If set to `false`, appends `DESC` to
-   * resulting Sql. If set to `null` then ommits sorting string.
+   * resulting Sql. If set to `null` then omits sorting string.
    */
   public function __construct(
     private readonly string $key,
@@ -17,8 +33,8 @@ final class SQLKeyPart
   )
   {
     $this->queryString = "$this->key";
-    if (!is_null($this->ascending))
-    {
+
+    if (!is_null($this->ascending)) {
       $this->queryString .= $this->ascending ? ' ASC' : ' DESC';
     }
   }
@@ -29,5 +45,13 @@ final class SQLKeyPart
   public function __toString(): string
   {
     return $this->queryString;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function jsonSerialize(): array
+  {
+    return [$this->key => $this->ascending ? 'ASC' : 'DESC'];
   }
 }
