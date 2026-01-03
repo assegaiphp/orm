2d9650b95edc26aa4efc3b0186aebfcd29ea210c
Andrew Masiye <amasiye313@gmail.com>
2026-01-03 18:31:42 +0200

    fix: fix SQLColumnDefinition conversion of numeric type enums to string

--- src/Queries/Sql/SQLColumnDefinition.php
+++ src/Queries/Sql/SQLColumnDefinition.php
@@ -8,11 +8,14 @@ use Stringable;
 
 class SQLColumnDefinition implements Stringable
 {
-  private string $queryString = "";
+  /**
+   * @var string
+   */
+  private string $queryString;
 
   /**
    * @param string $name
-   * @param string $type
+   * @param ColumnType $type
    * @param string|int|array|null $lengthOrValues
    * @param mixed|null $defaultValue
    * @param bool $nullable
@@ -40,8 +43,7 @@ class SQLColumnDefinition implements Stringable
   )
   {
     $queryString = !empty($this->name) ? "`$this->name` " : '';
-    if (is_null($this->lengthOrValues))
-    {
+    if (is_null($this->lengthOrValues)) {
       $this->lengthOrValues = match($this->type) {
         ColumnType::VARCHAR => '10',
         ColumnType::DECIMAL => '16,2',
@@ -49,20 +51,16 @@ class SQLColumnDefinition implements Stringable
       };
     }
 
-    if (!is_null($this->lengthOrValues))
-    {
+    if (!is_null($this->lengthOrValues)) {
       switch($this->type) {
         case ColumnType::TINYINT:
         case ColumnType::SMALLINT:
         case ColumnType::INT:
         case ColumnType::BIGINT:
-          $queryString .= $this->type;
-          if (!empty($this->lengthOrValues))
-          {
+          $queryString .= $this->type->value;
+          if (!empty($this->lengthOrValues)) {
             $queryString .= "(" . $this->lengthOrValues . ") ";
-          }
-          else
-          {
+          } else {
             $queryString .= " ";
           }
           break;
@@ -70,14 +68,11 @@ class SQLColumnDefinition implements Stringable
         case ColumnType::SMALLINT_UNSIGNED:
         case ColumnType::INT_UNSIGNED:
         case ColumnType::BIGINT_UNSIGNED:
-          $queryString .= $this->type;
-          if (!empty($this->lengthOrValues))
-          {
+          $queryString .= $this->type->value;
+          if (!empty($this->lengthOrValues)) {
             $length = $this->lengthOrValues;
             $queryString = str_replace(' UNSIGNED', "($length) UNSIGNED ", $queryString);
-          }
-          else
-          {
+          } else {
             $queryString .= " ";
           }
           break;
@@ -87,13 +82,11 @@ class SQLColumnDefinition implements Stringable
           break;
 
         case ColumnType::ENUM:
-          if (!is_array($this->lengthOrValues))
-          {
+          if (!is_array($this->lengthOrValues)) {
             $this->lengthOrValues = [];
           }
           $queryString .= $this->type->value . "(";
-          foreach ($this->lengthOrValues as $value)
-          {
+          foreach ($this->lengthOrValues as $value) {
             $queryString .= "'$value', ";
           }
           $queryString = trim($queryString, ', ');
@@ -102,29 +95,25 @@ class SQLColumnDefinition implements Stringable
   
         default: $queryString .= "{$this->type->value} ";
       }
-    }
-    else
-    {
+    } else {
       $queryString .= "{$this->type->value} ";
     }
 
-    if ($this->type->isNumeric() && is_string($this->defaultValue))
-    {
+    if ($this->type->isNumeric() && is_string($this->defaultValue)) {
       $this->defaultValue = $this->nullable || $this->autoIncrement ? null : 0;
     }
 
-    if (!is_null($this->defaultValue))
-    {
+    if (!is_null($this->defaultValue)) {
       $temporalDatatypes = [
         // SQLDataTypes::DATE,
         ColumnType::DATETIME
       ];
-      $stringExamptions = ['CURRENT_TIMESTAMP', 'CURRENT_DATE()', 'CURRENT_TIME()', 'JSON_ARRAY()'];
+      $stringExemptions = ['CURRENT_TIMESTAMP', 'CURRENT_DATE()', 'CURRENT_TIME()', 'JSON_ARRAY()'];
       $queryString .= "DEFAULT " . match(gettype($this->defaultValue)) {
         'object'  => method_exists($this->defaultValue, '__toString') ? strval($this->defaultValue) : json_encode($this->defaultValue),
         'boolean' => intval($this->defaultValue),
         // 'string'  => ( !in_array($this->dataType, $temporalDatatypes) ) ? "'" . $this->defaultValue . "'" : $this->defaultValue,
-        'string'  => !in_array($this->defaultValue, $stringExamptions) ? "'" . $this->defaultValue . "'" : (
+        'string'  => !in_array($this->defaultValue, $stringExemptions) ? "'" . $this->defaultValue . "'" : (
           match($this->defaultValue) {
             Column::CURRENT_DATE => "'" . date('Y-m-d') . "'",
             Column::CURRENT_TIME => "'" . date('H:i:s') . "'",
@@ -134,29 +123,23 @@ class SQLColumnDefinition implements Stringable
       } . " ";
     }
 
-    if ($this->autoIncrement && $this->type->isNumeric())
-    {
+    if ($this->autoIncrement && $this->type->isNumeric()) {
       $queryString .= "AUTO_INCREMENT ";
     }
 
     $queryString .= $this->nullable && !$this->isPrimaryKey ? "NULL " : "NOT NULL ";
 
-    if ($this->isPrimaryKey)
-    {
+    if ($this->isPrimaryKey) {
       $queryString .= "PRIMARY KEY ";
-    }
-    else if ($this->isUnique)
-    {
+    } else if ($this->isUnique) {
       $queryString .= trim("UNIQUE " . $this->uniqueKey) . ' ';
     }
 
-    if (!empty($this->onUpdate))
-    {
+    if (!empty($this->onUpdate)) {
       $queryString .= "ON UPDATE CURRENT_TIMESTAMP ";
     }
 
-    if (!empty($this->comment))
-    {
+    if (!empty($this->comment)) {
       $queryString .= "COMMENT $this->comment ";
     }
 
